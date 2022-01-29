889adb1def3e2cf304ec9749eb14719d6d1fcc94
AshishKumarJha <33698483+Oxymoron1234@users.noreply.github.com>
2022-01-30 02:31:34 +0530

    main

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import "./Navbar.css"
 const Navbar = () => {
+  const captureId = (e)=>{
+    console.log(e.target.value);
+  }
   return <>
-	<input type="checkbox" id="check"/>
-	<nav>
-		<div className="icon">Ass <b style={{color:"red"}}>N</b>T</div>
-		<div className="search_box">
-			<span className="fa fa-search"></span>
-			<input type="search" placeholder="Search"/>
-		</div>
-		<ol>
-			<li><a href="/"><i className="fa fa-building"/></a></li>
-		</ol>
-		<label htmlFor="check" className="bar">
-			<span className="fa fa-bars" id="bars"></span>
-			<span className="fa fa-times" id="times"></span>
-		</label>
-	</nav>
-	<section></section>
+    <input type="checkbox" id="check" />
+    <nav>
+      <div className="icon">
+      <select id="Building" onChange={captureId}>
+      <option value="0">Select Building</option>
+        <option value="1">Building 1</option>
+        <option value="2">Building 2</option>
+      </select>
+      </div>
+      <div className="search_box">
+        <span className="fa fa-search"></span>
+        <input type="search" placeholder="Search" />
+      </div>
+      <ol>
+        <li><a href="/"><i className="fa fa-building" /></a></li>
+      </ol>
+      <label htmlFor="check" className="bar">
+        <span className="fa fa-bars" id="bars"></span>
+        <span className="fa fa-times" id="times"></span>
+      </label>
+    </nav>
+    <section></section>
   </>
 };
 
--- src/components/main/MainBody.js
+++ src/components/main/MainBody.js
@@ -1,29 +1,29 @@
 import React,{useState, useEffect} from 'react';
 import Card from './Card';
 import Data from "../../data/indorPoss.json"
+import Dataposs from "../../data/indoor.json"
 const MainBody = () => {
     const [data, setData] = useState([])
-    const getCovidData =  () => {
+    const getJsonData =  () => {
         try {
-            
-            let jsonResult = JSON.stringify(Data)
-            console.log(jsonResult)
-            setData(jsonResult)
-           
-
+            const jsonResult = JSON.stringify(Dataposs) 
+              setData(jsonResult) 
+              console.log( Dataposs["1"][0])
         } catch (error) {
             console.log(error.message)
         }
 
     }
     useEffect(() => {
-        getCovidData();
+        getJsonData()
     }, [])
+    
   return <>
-        {
-            
-        }
-        <Card/>
+        <div className="main-body">
+            <div className="leftChild">hello</div>
+            <div className ="middleChild"> <Card/> </div>
+            <div className="rightChild">hello</div>
+        </div>
   </>;
 };
 
